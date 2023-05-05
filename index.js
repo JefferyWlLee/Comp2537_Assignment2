24ff64a1d4208a6c22d1bc84fb16632d55b72c2d
Jeff <jefferywllee@gmail.com>
2023-05-05 10:07:08 -0700

    added bootstrap and members page

--- index.js
+++ index.js
@@ -89,48 +89,6 @@ app.get('/', (req,res) => {
     res.render("index", {authenticated: req.session.authenticated});
 });
 
-app.get('/nosql-injection', async (req,res) => {
-	var username = req.query.user;
-
-	if (!username) {
-		res.send(`<h3>no user provided - try /nosql-injection?user=name</h3> <h3>or /nosql-injection?user[$ne]=name</h3>`);
-		return;
-	}
-	console.log("user: "+username);
-
-	const schema = Joi.string().max(20).required();
-	const validationResult = schema.validate(username);
-
-	//If we didn't use Joi to validate and check for a valid URL parameter below
-	// we could run our userCollection.find and it would be possible to attack.
-	// A URL parameter of user[$ne]=name would get executed as a MongoDB command
-	// and may result in revealing information about all users or a successful
-	// login without knowing the correct password.
-	if (validationResult.error != null) {  
-	   console.log(validationResult.error);
-	   res.send("<h1 style='color:darkred;'>A NoSQL injection attack was detected!!</h1>");
-	   return;
-	}	
-
-	const result = await userCollection.find({username: username}).project({username: 1, password: 1, _id: 1}).toArray();
-
-	console.log(result);
-
-    res.send(`<h1>Hello ${username}</h1>`);
-});
-
-app.get('/about', (req,res) => {
-    var color = req.query.color;
-
-    res.render("about", {color: color});
-});
-
-app.get('/contact', (req,res) => {
-    var missingEmail = req.query.missing;
-
-    res.render("contact", {missing: missingEmail});
-});
-
 app.post('/submitEmail', (req,res) => {
     var email = req.body.email;
     if (!email) {
@@ -232,30 +190,11 @@ app.get('/logout', (req,res) => {
 });
 
 
-app.get('/cat/:id', (req,res) => {
-    var cat = req.params.id;
-
-    res.render("cat", {cat: cat});
-});
-
 
 app.get('/admin', sessionValidation, adminAuthorization, async (req,res) => {
-	if (req.session.authenticated){
 		const result = await userCollection.find().project({username: 1, _id: 1, user_type: 1}).toArray();
-		const promote = async (user) => {
-            db.user.updateOne({username: user}, {$set: {user_type:"admin"}});
-        };
-		const demote = async (user) => {
-            db.user.updateOne({username: user}, {$set: {user_type:"user"}});
-        };
 		console.log("test");
-    	res.render("admin", {users: result, promote: promote, demote: demote});
-	} else {
-		req.session.errorMessage = "not logged in, please login";
-		res.redirect("/login");
-		
-	}
-    
+    	res.render("admin", {users: result});
 });
 
 app.get('/admin/demote', (req,res) => {
@@ -268,6 +207,10 @@ app.get('/admin/promote', (req,res) => {
 	var user = req.query.user;
 	userCollection.updateOne({username:user}, {$set: {user_type:"admin"}});
 	res.redirect("/admin");
+})
+
+app.get('/member', (req,res) => {
+	res.render("members");
 })
 
 app.use(express.static(__dirname + "/public"));
